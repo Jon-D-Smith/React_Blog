343b1b253ce0c2cfbe470e1f8efa6f37ab3c8c97
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-22 13:32:37 -0500

    Practiced filtering with props

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,14 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: "My new website!", body: "lorem ipsum text...", author: "Jon", id: 1 },
         {title: "My second dummy post!", body: "lorem ipsum text...", author: "Reagann", id: 2 },
-        {title: "Feed me", body: "lorem ipsum text...", author: "BlueBelle", id: 3 }
+        {title: "Feed me", body: "lorem ipsum text...", author: "BlueBelle", id: 3 },
+        {title: "Jon's second post", body: "lorem ipsum text", author: "Jon", id: 4}
     ])
 
     return ( 
         <div className="home">
-                    <Bloglist blogs={blogs} title="All Blogs"/> 
+                <Bloglist blogs={blogs} title="All Blogs"/> 
+                <Bloglist blogs={blogs.filter((blog) => blog.author === "Jon")} title="Jon's Blogs" />
         </div>
      );
 }
