d0ef2d80587c7b708178f3f8e8dbdfbf81f27bde
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-22 13:16:45 -0500

    Added blog previews

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,21 @@
 import {useState} from 'react'
 const Home = () => {
-    const [name, setName] = useState('Mario')
-    const [age, setAge] = useState(27)
-
-    const handleClick = (e) => {
-        setName('luigi')
-        setAge(28)
-        console.log(name)
-    }
-
-
+  
+    const [blogs, setBlogs] = useState([
+        {title: "My new website!", body: "lorem ipsum text...", author: "Jon", id: 1 },
+        {title: "My second dummy post!", body: "lorem ipsum text...", author: "Reagann", id: 2 },
+        {title: "Feed me", body: "lorem ipsum text...", author: "BlueBelle", id: 3 }
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-            <p>{age}</p>
-            <button onClick={handleClick}>Click me!</button>
-            
+                  {blogs.map((blog) =>(
+                      <div key={blog.id} className="blog-preview">
+                        <h2>{blog.title}</h2>
+                        <p>Written By: {blog.author}</p>
+                      </div>
+                      
+                   ) )}
         </div>
      );
 }
